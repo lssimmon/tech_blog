6178dd80af1e640d35a83b0e79da5bfb38ec3a2f
Lauryn Simmons <laurynssimmons@gmail.com>
2023-08-23 10:07:16 -0400

    Actions and config stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-  root to: "main#index", as: :main_index
+  root to: 'main#index', as: :main_index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
